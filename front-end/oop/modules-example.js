6e1788d5b72a83a6c9548ac9b9590c1120b1a7dd
RAJESH2132 <rajeshnagurla@gmail.com>
2024-11-13 14:13:49 +0530

    module examples

---
+++ front-end/oop/modules-example.js
@@ -0,0 +1,12 @@
+export function Add(a = 10, b = 20) {
+  return a + b;
+}
+export function Multiplication(a = 5, b = 10) {
+  return a * b;
+}
+export default function Name(name = "Rajesh") {
+  return name;
+}
+export function Hello(name = "Rajesh") {
+  return `Hello, ${name}`;
+}
